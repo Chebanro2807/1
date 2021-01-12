5eb15a238eff7b02fc0241f6e1d239bb55603216
Chebanro2807 <chebanro2807@ukr.net>
2021-01-12 15:21:27 +0200

    create a field

---
+++ Sapper/field.js
@@ -0,0 +1,16 @@
+function fieldInit() {
+    let _rootEl = document.querySelector(".cross__board");
+    for (let i=0; i<=63; i++) {
+        let squere = document.createElement('div');
+        squere.className='cross__board-item';
+        squere.setAttribute("data-index", i );
+        let bomb = document.createElement('img');
+        bomb.className='bomb';
+        bomb.setAttribute("src", "../Sapper/img/bomb.png" );
+        squere.append(bomb);
+        _rootEl.append(squere);
+        
+
+    }
+}
+fieldInit();
