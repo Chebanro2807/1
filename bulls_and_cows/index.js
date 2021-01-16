eb33f56f51ff391b23ec39cbfb05ebeda8364e12
Chebanro2807 <chebanro2807@ukr.net>
2021-01-16 15:53:35 +0200

    bulls and cows add mechanic

--- bulls_and_cows/index.js
+++ bulls_and_cows/index.js
@@ -4,6 +4,7 @@ function BullsAndCows (props = {}){
     this.createField()
     this.startGame = document.querySelector('.button__start');
     this.startGame.addEventListener('click',this.start.bind(this));
+    this._currentTurn = 0;
 }
 // Generate field
 BullsAndCows.prototype.randomNumber = function (){
@@ -13,7 +14,7 @@ BullsAndCows.prototype.randomNumber = function (){
 BullsAndCows.prototype.randomNumberToString = function() {
     do {
         this._string = `${this.randomNumber().toString().padStart(4, '0')}`;
-    } while(!this.verification());
+    } while(!this.verification(this._string));
 }
  
 BullsAndCows.prototype.render = function() {
@@ -23,11 +24,11 @@ BullsAndCows.prototype.render = function() {
     }
 }
 
-BullsAndCows.prototype.verification = function (){
-    console.log(this._string);
-    for (let i=0; i<=3; i++){
-        for(let j=i+1; j<=3; j++){
-            if (this._string[i]===this._string[j]){
+BullsAndCows.prototype.verification = function (check){
+    // console.log(check);
+    for (let i=0; i<4; i++){
+        for(let j=i+1; j<4; j++){
+            if (check[i]===check[j]){
                 return false;
             }
         }
@@ -35,10 +36,38 @@ BullsAndCows.prototype.verification = function (){
     return true;
 }
 
+BullsAndCows.prototype.checkBullsAndCows = function (usernumber) {
+    let bulls = 0;
+    let cows = 0;
+    for (let i=0; i<4; i++){
+        for (let j=0; j<4; j++){
+            if (usernumber[i]===this._string[j]){
+                if (i===j) {continue}
+                cows++
+            }
+        }
+        if (usernumber[i]===this._string[i]){
+            bulls++
+        }
+        
+    }
+    console.log('быки =', bulls, 'коровы =',cows)
+    this.drow(bulls, cows);
+}
+
+BullsAndCows.prototype.drow = function (bullsQuantity, cowsQuantity){
+    let drawBulls = document.querySelector('.bull_'+this._currentTurn);
+    let drawCows = document.querySelector('.cow_'+this._currentTurn);
+    drawBulls.innerHTML = bullsQuantity;
+    drawCows.innerHTML = cowsQuantity;
+}
+
 BullsAndCows.prototype.start = function() {
     this.fieldClean();
     this.randomNumberToString();
-    this.render ();
+    this.render();
+    this._currentTurn = 0;
+    this.createInputNumber();
 }
 
 BullsAndCows.prototype.createField = function () {
@@ -62,6 +91,35 @@ BullsAndCows.prototype.createField = function () {
         createTable.append(row);
     }
 }
+
+BullsAndCows.prototype.createInputNumber = function (){
+    let cellNumberTd = document.querySelector('.number_'+this._currentTurn);
+    let createInput = document.createElement('input')
+    createInput.setAttribute('size',2);
+    createInput.setAttribute('maxlength',4)
+    // createInput.setAttribute('pattern','[0-9]{4}') // Разобраться с данным свойством немного позже. 
+    createInput.addEventListener('keypress', (event) => {
+        if (event.keyCode ===13) {
+            event.preventDefault()
+            if (this.verification(createInput.value)) {
+                createInput.setAttribute('disabled','disabled')
+                this.checkBullsAndCows(createInput.value);
+                this._currentTurn++;
+                if (this._currentTurn===10){
+                    alert('Ты нубас')
+
+                } else {
+                    this.createInputNumber();
+                }
+            } else {
+                alert('У вас повторяються цыфры. \n"1234" => Правильно, \n"1123" =>  Не верно. В данном случае дважди повторилось "1 и 1"')
+            }
+        }
+    });
+    cellNumberTd.append(createInput)
+}
+
+
 
 BullsAndCows.prototype.fieldClean = function() {
 
